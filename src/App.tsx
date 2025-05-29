e843145976f3bb1451ab53897d788924a7220d73
Vuthuru Srivardhan <vuthurusrivardhan@gmail.com>
2025-05-29 17:10:02 +0530

    Intilized the app and add shadcn

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+
+function App() {
+  return (
+    <div className="">
+      <Button variant="outline">Button</Button>
+    </div>
+  )
+}
+
+export default App
